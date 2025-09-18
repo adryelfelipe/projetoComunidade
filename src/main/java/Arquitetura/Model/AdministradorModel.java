5603d5281b840713f718326af976c58118d40f87
Adryel Felipe <adryxd003@gmail.com>
2025-09-18 20:30:33 -0300

    ✨ Fit: Adiciona a classe AdministradorModel
    Adiciona a Model Usuario com os atributos de Usuario

---
+++ src/main/java/Arquitetura/Model/AdministradorModel.java
@@ -0,0 +1,16 @@
+package Arquitetura.Model;
+
+public class AdministradorModel extends UsuarioModel{
+
+    // Atributos
+
+    // Construtor
+    public AdministradorModel(String nome, String cpf, String senha)
+    {
+        super(nome, cpf, senha);
+    }
+
+    // Setters e Getters
+
+    // Métodos
+}
