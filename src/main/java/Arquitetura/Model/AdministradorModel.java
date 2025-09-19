8e733e608be823791adcdc65d7891614fddb0927
EDUARDO GEFFERT DA SILVA <eduardo_silva173@estudante.sesisenai.org.br>
2025-09-19 20:13:10 -0300

    ✨ Fit: Modifica a classe Administrador
    Modifica a model Administrador adcionando os atributos especificos

--- src/main/java/Arquitetura/Model/AdministradorModel.java
+++ src/main/java/Arquitetura/Model/AdministradorModel.java
@@ -3,16 +3,35 @@ package Arquitetura.Model;
 public class AdministradorModel extends UsuarioModel{
 
     // Atributos
+    private double salario;
+    private int cargaHoraria;
 
     // Construtor
-    public AdministradorModel(String nome, String cpf, String senha)
+    public AdministradorModel(String nome, String cpf, String senha, double salario, int cargaHoraria)
     {
         super(nome, cpf, senha);
-
+        this.salario = salario;
+        this.cargaHoraria = cargaHoraria;
         this.tipoUsuario = "Administrador";
     }
 
     // Setters e Getters
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
 
     // Métodos
+
 }
