3413935fb998edb4d84ced38b6239ba02c9273d5
EDUARDO GEFFERT DA SILVA <eduardo_silva173@estudante.sesisenai.org.br>
2025-09-19 20:26:54 -0300

    ✨ Fit: Adiciona a classe Medico
    Adiciona a model Medico com todos os atributos especificos

---
+++ src/main/java/Arquitetura/Model/MedicoModel.java
@@ -0,0 +1,84 @@
+package Arquitetura.Model;
+
+public class MedicoModel extends UsuarioModel{
+
+    //Atributos
+    private double salario;
+    private int cargaHorariaSemanal;
+    private String plantao;
+    private String especialidade;
+    private String subEspecialidade;
+    private String formacao;
+
+    //Construtores
+
+    // Não possui subEspecialidade
+    public MedicoModel(String nome, String cpf, String senha, double salario, int cargaHorariaSemanal, String plantao, String especialidade, String formacao) {
+        super(nome,cpf,senha);
+        this.cargaHorariaSemanal = cargaHorariaSemanal;
+        this.salario = salario;
+        this.plantao = plantao;
+        this.especialidade = especialidade;
+        this.formacao = formacao;
+        this.tipoUsuario = "Médico";
+        this.subEspecialidade = "N/A";
+    }
+
+    // Possui subEspecialidade
+    public MedicoModel(String nome, String cpf, String senha, double salario, int cargaHorariaSemanal, String plantao, String especialidade, String formacao, String subEspecialidade) {
+        this(nome, cpf, senha, salario, cargaHorariaSemanal, plantao, especialidade, formacao);
+        this.subEspecialidade = subEspecialidade;
+    }
+
+    //Getters & Setters
+    public void setCargaHorariaSemanal(int cargaHorariaSemanal) {
+        this.cargaHorariaSemanal = cargaHorariaSemanal;
+    }
+
+    public int getCargaHorariaSemanal() {
+        return cargaHorariaSemanal;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+
+    public String getFormacao() {
+        return formacao;
+    }
+
+    public void setPlantao(String plantao) {
+        this.plantao = plantao;
+    }
+
+    public String getPlantao() {
+        return plantao;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSubEspecialidade(String subEspecialidade) {
+        this.subEspecialidade = subEspecialidade;
+    }
+
+    public String getSubEspecialidade() {
+        return subEspecialidade;
+    }
+
+    //Métodos
+
+}
