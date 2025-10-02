7f78f176ba0ca4b20461b3ff8991ad8452cafb6d
Adryel Felipe <adryxd003@gmail.com>
2025-10-02 14:00:05 -0300

    ✨ Fit: Adiciona níveis de acesso na enum NivelAcesso
    Objetos estáticos: N1, N2, N3. Também adiciona métodos para verificar o nivel de acesso

--- src/main/java/Arquitetura/Model/Enums/NivelAcesso.java
+++ src/main/java/Arquitetura/Model/Enums/NivelAcesso.java
@@ -1,4 +1,36 @@
 package Arquitetura.Model.Enums;
 
 public enum NivelAcesso {
+
+    // -- Objetos Estáticos Finais -- //
+
+    // ACESSO BAIXO
+    N1,
+
+    // ACESSO MODERADO
+    N2,
+
+    // ACESSO TOTAL
+    N3;
+
+    // -- Métodos que checam o acesso -- //
+
+    // Retorna true se o usuario tem acesso total (N3)
+    public boolean temAcessoTotal() {
+
+        return this == N3;
+    }
+
+    // Retorna true se o usuario tem acesso moderado (N3 e N2)
+    public boolean temAcessoModerado() {
+
+        return (this == N2 || this == N3);
+    }
+
+    // Retorna true se o usuario tem acesso moderado (N3, N2 e N1)
+    public boolean temAcessoBaixo() {
+
+        return (this == N1 || this == N2 || this == N3);
+    }
+
 }
