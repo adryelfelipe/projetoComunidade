9553a4742f360e34a3d73cf88a81eea7abcfb542
Adryel Felipe <adryxd003@gmail.com>
2025-10-03 13:55:39 -0300

    ✨ Fit: Adiciona a classe MenuDefault
    Possui método static menuDefault() com o menu para opções incorretas

---
+++ src/main/java/Arquitetura/View/MenuDefault.java
@@ -0,0 +1,14 @@
+package Arquitetura.View;
+
+import Arquitetura.Utilidades.Ferramentas;
+
+public class MenuDefault {
+
+    // -- MÉTODOS ESTÁTICOS -- //
+    public static void menuDefault() {
+        System.out.println("-------------------------\n");
+        System.out.println("Valor digitado incorreto!");
+        System.out.println("-------------------------\n");
+        Ferramentas.Delay(1500);
+    }
+}
