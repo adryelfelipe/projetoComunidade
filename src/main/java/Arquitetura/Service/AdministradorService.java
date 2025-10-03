cfd3aa51fa54af1920787b361d863791757e1bf3
Adryel Felipe <adryxd003@gmail.com>
2025-10-03 11:19:10 -0300

    ✨ Fit: Separa a verificação de isUltimoAdmin em um método em AdministradorService
    Remove a verificação do método verificarDadosAdm() e cria um método separado isUltimoAdmin(). Também altera deletarAdministrador para suportar as mudanças

--- src/main/java/Arquitetura/Service/AdministradorService.java
+++ src/main/java/Arquitetura/Service/AdministradorService.java
@@ -20,9 +20,14 @@ public class AdministradorService {
 
     // -- Métodos -- //
 
+    // Verifica se é o último administrador do banco de dados
+    private boolean isUltimoAdmin(Administrador administrador) {
+        return administradorDao.isUltimoAdmin();
+    }
+
     // Verifica a veracidade dos atributos específicos de Admin
     public boolean verificarDadosAdm(Administrador administrador) {
-        return !administradorDao.isUltimoAdmin(); //provisório
+       return true; // provisório
     }
 
     // Insere o objeto do tipo Administrador no banco de dados
@@ -40,7 +45,7 @@ public class AdministradorService {
 
     // Deleta um administrador do banco de dados
     public boolean deletarAdministrador(Administrador adminUsado, Administrador adminDeletado) {
-        if(!(adminUsado == adminDeletado)) {
+        if(!(adminUsado == adminDeletado) && !isUltimoAdmin(adminDeletado)) {
             if(funcionarioService.deletarFuncionario(adminDeletado.getId())) {
                 administradorDao.deletarAdministrador(adminDeletado.getId());
                 funcionarioDAO.deletarFuncionario(adminDeletado.getId());
