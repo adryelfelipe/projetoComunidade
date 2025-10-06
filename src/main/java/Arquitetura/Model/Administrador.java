0e7c3e183a7d2de856f47bb022ba79b769347098
Adryel Felipe <adryxd003@gmail.com>
2025-10-06 10:43:57 -0300

    ✨ Fit: Altera a model Administrador para suportar a enum Departamento
    Muda o atributo departamento de String para Departamentow. Adiciona método getIdDepartamento

--- src/main/java/Arquitetura/Model/Administrador.java
+++ src/main/java/Arquitetura/Model/Administrador.java
@@ -1,5 +1,6 @@
 package Arquitetura.Model;
 
+import Arquitetura.Model.Enums.Departamento;
 import Arquitetura.Model.Enums.TipoUsuario;
 
 import java.sql.Date;
@@ -7,29 +8,46 @@ import java.sql.Date;
 public class Administrador extends Funcionario {
 
     // -- Atributos -- //
-    private String departamento;
+    private Departamento departamento;
 
     // -- Construtores -- //
 
     // Construtor sem id
-    public Administrador(String nome, String cpf, String senha, String sexo, String telefone, String email,Date dataNascimento, double salario, int cargaHorariaSemanal, String departamento)
+    public Administrador(String nome, String cpf, String senha, String sexo, String telefone, String email,Date dataNascimento, double salario, int cargaHorariaSemanal, Departamento departamento)
     {
         super(TipoUsuario.ADMIN, nome, cpf, senha, sexo, telefone, email, dataNascimento, salario, cargaHorariaSemanal);
         this.departamento = departamento;
     }
 
     // Construtor com id
-    public Administrador(String nome, String cpf, String senha, String sexo, String telefone,String email,Date dataNascimento,double salario, int cargaHoraria,String departamento, long id) {
+    public Administrador(String nome, String cpf, String senha, String sexo, String telefone,String email,Date dataNascimento,double salario, int cargaHoraria,Departamento departamento, long id) {
         this(nome, cpf, senha, sexo, telefone,email, dataNascimento, salario, cargaHoraria, departamento);
         this.setId(id);
     }
 
     // --  Setters e Getters -- //
-    public String getDepartamento() {
+    public Departamento getDepartamento() {
         return departamento;
     }
 
-    public void setDepartamento(String departamento) {
+    public void setDepartamento(Departamento departamento) {
         this.departamento = departamento;
+    }
+
+    public long getIdDepartamento() {
+
+        if(departamento == Departamento.FINANCEIRO) {
+
+            return 1;
+        } else if(departamento == Departamento.INFRAESTRUTURA) {
+
+            return 2;
+        } else if(departamento == Departamento.MARKETING) {
+
+            return 3;
+        } else {
+
+            return 4;
+        }
     }
 }
