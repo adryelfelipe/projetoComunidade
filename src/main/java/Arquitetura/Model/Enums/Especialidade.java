e59538a351a2626ac736971dda415b258b717d23
Adryel Felipe <adryxd003@gmail.com>
2025-10-08 21:15:15 -0300

    ✨ Fit: Adiciona atributo do tipo List<Exame> de lista de Exames na enum Especialidade
    Adiciona atributo e altera construtor para receber um conjunto de exames em forma de uma array (varArgs), depois converte em List com Arrays.asList

--- src/main/java/Arquitetura/Model/Enums/Especialidade.java
+++ src/main/java/Arquitetura/Model/Enums/Especialidade.java
@@ -1,38 +1,47 @@
 package Arquitetura.Model.Enums;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public enum Especialidade {
 
     // -- Objetos Estáticos -- //
 
     // ID 1
-    CLINICO_GERAL("Responsável por: Hemograma, Glicemia, Colesterol, Sangue", 1),
+    CLINICO_GERAL("Responsável por: Hemograma, Glicemia, Colesterol, Sangue", 1, Exame.Hemograma, Exame.Glicemia, Exame.Colesterol, Exame.Sangue),
 
     // ID 2
-    CARDIOLOGISTA("Responsável por: Eletrocardiograma, Teste Ergométrico", 2),
+    CARDIOLOGISTA("Responsável por: Eletrocardiograma, Teste Ergométrico", 2, Exame.Eletrocardiograma, Exame.TesteErgometrico),
 
     // ID 3
-    RADIOLOGISTA("Responsável por: Raio-X",3),
+    RADIOLOGISTA("Responsável por: Raio-X",3, Exame.RaioX),
 
     // ID 4
-    OTORRINOLARINGOLOGISTA("Responsável por: Audiometria, Áudio",4),
+    OTORRINOLARINGOLOGISTA("Responsável por: Audiometria, Áudio",4, Exame.Audiometria, Exame.Audio),
 
     // ID 5
-    OFTALMOLOGISTA("Responsável por: Visão",5),
+    OFTALMOLOGISTA("Responsável por: Visão",5, Exame.Visao),
 
     // ID 6
-    ENDOCRINOLOGISTA("Responsável por: Glicemia, Colesterol", 6),
+    ENDOCRINOLOGISTA("Responsável por: Glicemia, Colesterol", 6, Exame.Glicemia, Exame.Colesterol),
 
     // ID 7
-    HEMATOLOGISTA("Responsável por: Hemograma, Sangue", 7);
+    HEMATOLOGISTA("Responsável por: Hemograma, Sangue", 7, Exame.Hemograma, Exame.Sangue);
 
     // -- Atributos extras -- //
     private String descricao;
     private final long idEspecialidade;
+    private final List<Exame> exames;
 
     // -- Construtor -- //
-    Especialidade(String descricao, long idEspecialidade) {
+    Especialidade(String descricao, long idEspecialidade, Exame... arrayExames)
+    {
         this.descricao = descricao;
         this.idEspecialidade = idEspecialidade;
+
+        // Converte uma array símples em uma Lista
+        exames = Arrays.asList(arrayExames);
     }
 
     // - Setters e Getters -- //
@@ -43,6 +52,7 @@ public enum Especialidade {
 
     public long getIdEspecialidade()
     {
+
         return idEspecialidade;
     }
 
@@ -51,43 +61,12 @@ public enum Especialidade {
     }
 
     // -- Métodos -- //
-    public boolean realizaHemograma() {
-        return (this == CLINICO_GERAL || this == HEMATOLOGISTA);
-    }
+    public boolean podeRealizar(Exame exame) {
 
-    public boolean realizaGlicemia() {
-        return (this == CLINICO_GERAL || this == ENDOCRINOLOGISTA);
-    }
+        return this.exames.contains(exame);
+   }
 
-    public boolean realizaColesterol() {
-        return (this == CLINICO_GERAL || this == ENDOCRINOLOGISTA);
-    }
-
-    public boolean realizaRaioX() {
-        return (this == RADIOLOGISTA);
-    }
-
-    public boolean realizaEletrocardio() {
-        return (this == CARDIOLOGISTA);
-    }
-
-    public boolean realizaTesteErgo() {
-        return (this == CARDIOLOGISTA);
-    }
-
-    public boolean realizaAudiometria() {
-        return (this == OTORRINOLARINGOLOGISTA);
-    }
-
-    public boolean realizaAudio() {
-        return (this == OTORRINOLARINGOLOGISTA);
-    }
-
-    public boolean realizaVisao() {
-        return (this == OFTALMOLOGISTA);
-    }
-
-    public boolean realizaSangue() {
-        return (this == CLINICO_GERAL || this == HEMATOLOGISTA);
-    }
+   public void adicionarExame(Exame exame) {
+        this.exames.add(exame);
+   }
 }
