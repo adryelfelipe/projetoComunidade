9b5dd707612514004bcdfb435690121e4fe7976c
Adryel Felipe <adryxd003@gmail.com>
2025-10-09 21:09:24 -0300

    ✨ Fit: Adiciona PacienteValidator
    Possui método de verificar os dados do paciente, verificarDadosPaciente()

--- src/main/java/Arquitetura/Service/Validator/PacienteValidator.java
+++ src/main/java/Arquitetura/Service/Validator/PacienteValidator.java
@@ -1,6 +1,23 @@
 package Arquitetura.Service.Validator;
 
+import Arquitetura.Model.Enums.StatusPaciente;
+import Arquitetura.Model.Paciente;
+import Arquitetura.Model.Usuario;
+
 public class PacienteValidator {
 
+    // -- Métodos de verificação -- //
+    public void verificarDadosPaciente(Paciente paciente) {
+        if(paciente.getStatusPaciente() == null) {
+            throw new IllegalArgumentException("ERRO! O STATUS DO PACIENTE NÃO PODE SER NULO");
+        }
 
+        if(paciente.getNumeroCarterinha() == null) {
+            throw new IllegalArgumentException("ERRO! O NÚMERO DA CARTERINHA NÃO PODE SER NULO");
+        }
+
+        if(paciente.getContatoEmergencia() == null) {
+            throw new IllegalArgumentException("ERRO! O CONTATO DE EMERGÊNCIA NÃO PODE SER NULO");
+        }
+    }
 }
