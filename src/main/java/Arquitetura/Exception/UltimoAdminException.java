20a317133903c60b94aa2c1fdbcfa4eeb77efa37
Adryel Felipe <adryxd003@gmail.com>
2025-10-14 00:36:56 -0300

    ✨ Fit: Adiciona a classe UltimoAdminException
    Exceção criada para indicar falhas ao tentar excluir o último administrador do DB

---
+++ src/main/java/Arquitetura/Exception/UltimoAdminException.java
@@ -0,0 +1,9 @@
+package Arquitetura.Exception;
+
+public class UltimoAdminException extends RuntimeException {
+
+    // -- Construtor -- //
+    public UltimoAdminException(String message) {
+        super(message);
+    }
+}
