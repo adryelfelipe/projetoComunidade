2ba080ada61f453e7a25fea5ac8cab2615693567
Adryel <adryxd003@gmail.com>
2025-10-14 16:42:20 -0300

    ✨ Fit: Altera a FuncionarioService para suportar o novo fluxo de inserção e validators
    Remove o método de inserir para evitar que alguém inserisse apenas um funcionário e um usuário ao chamar esse método em qualquer lugar do código e implementa a FuncionarioValidator

--- src/main/java/Arquitetura/Service/FuncionarioService.java
+++ src/main/java/Arquitetura/Service/FuncionarioService.java
@@ -6,44 +6,5 @@ import Arquitetura.Model.Funcionario;
 import Arquitetura.Model.Usuario;
 
 public class FuncionarioService {
-
-    // -- Atributos -- //
-    private final UsuarioService usuarioService = new UsuarioService();
-    private final FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-
-    // -- Construtor -- //
-    public FuncionarioService() {
-
-    }
-
-    // -- Métodos -- //
-
-    // Verifica os dados gerais de funcionários
-    private boolean verificarDadosFunc(Funcionario funcionario) {
-
-        return (funcionario.getCargaHorariaSemanal() >= 40 && funcionario.getSalario() > 0);
-    }
-
-    // Insere os atributos gerais de Funcionario na tabela Funcionario do banco de dados
-    public boolean inserirFuncionario(Usuario usuario, Funcionario funcionario) {
-        if(usuario.getTipoUsuario().getNivelAcesso().temAcessoTotal()) {
-            if (verificarDadosFunc(funcionario)) { // Verifica os dados de Funcionario
-                if (usuarioService.inserirUsuario(usuario, funcionario)) { // Verifica os dados de Usuario
-                    funcionarioDAO.inserirFuncionario(funcionario);
-
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    // Cojunto de regras de negócio gerais para deletar qualquer tipo de funcionario
-    boolean deletarFuncionario(long id) {
-
-        return usuarioService.deletarUsuario(id);
-
-        // Somente verificações gerais por enquanto
-    }
+    // NO FUTURO ADICIONAR VERIFICAÇÕES QUE EXIGAM DAOS
 }
