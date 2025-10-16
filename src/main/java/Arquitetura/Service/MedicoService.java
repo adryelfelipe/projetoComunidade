6c90bbd15237ee9a911308f74b7665d7a11a3674
Adryel Felipe <adryxd003@gmail.com>
2025-10-16 18:09:25 -0300

    📝 Docs: Adiciona documentação (Javadoc) na classe MedicoService
    Documenta os métodos de inserir e deletar

--- src/main/java/Arquitetura/Service/MedicoService.java
+++ src/main/java/Arquitetura/Service/MedicoService.java
@@ -10,6 +10,8 @@ import Arquitetura.Model.Usuario;
 import Arquitetura.Service.Validator.MedicoValidator;
 import Arquitetura.Service.Validator.TipoUsuarioValidator;
 import Arquitetura.Service.Validator.UsuarioValidator;
+import Arquitetura.Exception.TipoUsuarioException;
+import Arquitetura.Exception.DadosInvalidosException;
 
 public class MedicoService {
 
@@ -30,7 +32,20 @@ public class MedicoService {
 
     // -- Métodos -- //
 
-    // Insere o objeto do tipo Medico no banco de dados
+    /**
+     * <p>Este método realiza as seguintes ações: </p>
+     *
+     * <ol>
+     *     <li>Verifica se o usuário possui permissão para inserir</li>
+     *     <li>Verifica se o médico inserido segue as regras gerais de inserção de um usuário </li>
+     *     <li>Verifica se os dados do médico</li>
+     *     <li>Insere o médico nas tabelas: Usuario, Funcionario e Medico respectivamente</li>
+     * </ol>
+     * @param usuario Quem está inserindo
+     * @param medicoInserido Quem será inserido
+     * @throws TipoUsuarioException Se o usuario não possuir acesso total (necessário para inserção)
+     * @throws DadosInvalidosException Se os dados do medico não seguirem as regras de negócio
+     */
     public void inserirMedico(Usuario usuario, Medico medicoInserido) {
         // Verificações de dados
         tipoUsuarioValidator.temAcessoTotal(usuario);
@@ -43,6 +58,20 @@ public class MedicoService {
         medicoDAO.inserirMedico(medicoInserido);
     }
 
+    /**
+     * <p>Este método realiza as seguintes ações: </p>
+     *
+     *<ol>
+     *     <li>Verifica se o usuario possui acesso para deletar</li>
+     *     <li>Verifica se o CPF inserido existe</li>
+     *     <li>Deleta o médico das tabelas Medico, Funcionario e Usuario do banco de dados</li>
+     *</ol>
+     *
+     * @param usuario Quem está deletando
+     * @param cpfMedicoDeletado CPF de quem será deletado
+     * @throws TipoUsuarioException Se o usuario não possuir acesso total (necessário para deletar)
+     * @throws CpfInvalidoException Se o cpf não existir no banco de dados
+     */
     // Deleta medico do banco de dados
     public void deletarMedico(Usuario usuario, String cpfMedicoDeletado) {
         // Verificação de dados
