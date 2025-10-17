ee3151e340706c410ccf01dc38e2b787f46f5717
EDUARDO GEFFERT DA SILVA <eduardo_silva173@estudante.sesisenai.org.br>
2025-10-16 21:37:16 -0300

    ✨ Fit: Adiciona Função Listar consultas do MenuMinhaAgenda

--- src/main/java/Arquitetura/View/FuncoesMedico/MenuMinhaAgenda.java
+++ src/main/java/Arquitetura/View/FuncoesMedico/MenuMinhaAgenda.java
@@ -1,9 +1,11 @@
 package Arquitetura.View.FuncoesMedico;
 
+import Arquitetura.Dao.PacienteDAO;
 import Arquitetura.Model.Consulta;
 import Arquitetura.Model.Enums.StatusConsulta;
 import Arquitetura.Model.Medico;
 import Arquitetura.Service.MedicoService;
+import Arquitetura.Service.PacienteService;
 import Arquitetura.Utilidades.Ferramentas;
 
 import java.util.ArrayList;
@@ -12,29 +14,31 @@ public class MenuMinhaAgenda
 {
 
         static MedicoService medicoService = new MedicoService();
+        static PacienteService pacienteService = new PacienteService();
+        static PacienteDAO pacienteDAO = new PacienteDAO();
 
         public static void MinhaAgenda(Medico medico)
         {
 
              Ferramentas.limpaTerminal();
 
-            ArrayList<Consulta> consultas = medicoService.ConsultasMedico(medico,medico.getCpf());
+
 
             System.out.println("         ----------------------");
             System.out.println("         -- MINHAS CONSULTAS --");
             System.out.println("         ----------------------");
 
             System.out.println("\n\n\n");
-            for (Consulta entrad: consultas)
-            {
-                if (entrad.getStatus().equals(StatusConsulta.AGENDADA)) {
-                    System.out.println(entrad.getIdConsulta());
-                    System.out.println(entrad.getDataConsulta());
-                    System.out.println(entrad.getHorarioConsulta());
-                    System.out.println(entrad.getExame());
-                    System.out.println(entrad.getPaciente());
-                }
-            }
+            //for (Consulta entrad: consultas)
+            //{
+            //    if (entrad.getStatus().equals(StatusConsulta.AGENDADA)) {
+            //        System.out.println(entrad.getIdConsulta());
+            //        System.out.println(entrad.getDataConsulta());
+            //        System.out.println(entrad.getHorarioConsulta());
+            //        System.out.println(entrad.getExame());
+            //        System.out.println(entrad.getPaciente());
+            //    }
+            //}
 
             System.out.println("\n\nDigite para continuar");
             String tempo = Ferramentas.lString();
