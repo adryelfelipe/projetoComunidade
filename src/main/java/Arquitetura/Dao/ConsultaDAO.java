25164ff107abe3c48a97a3060b3eced5dfb9654b
GUSTAVO DE HONORATO PELISSARI <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-17 15:48:31 -0300

    ✨ Fit: Adiciona função deletarConsultaWhereCpfPaciente em ConsultaDAO

--- src/main/java/Arquitetura/Dao/ConsultaDAO.java
+++ src/main/java/Arquitetura/Dao/ConsultaDAO.java
@@ -77,6 +77,24 @@ public class ConsultaDAO
         }
     }
 
+    public void deletarConsultaWhereCpfPaciente(String cpf)
+    {
+        String querySql = "DELETE c"+
+                "FROM Consulta c "+
+                "INNER JOIN Usuario u ON c.idPaciente = u.idUsuario "+
+                "WHERE u.cpf = ?";
+        try(
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(querySql))
+        {
+            stmt.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Não foi possível deletar Consulta onde CPF do Paciente é igual: "+cpf+ e);
+        }
+    }
+
     public Consulta findById(long id)
     {
         String querySQL = "SELECT idConsulta, dataConsulta, horarioConsulta, relatorio, idPaciente, idMedico, idExame, idStatus "+
