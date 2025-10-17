07e7b4beaed0fdba5a13d7cbe39d76a1d8b2d4e5
GUSTAVO DE HONORATO PELISSARI <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-17 20:13:09 -0300

    ♻️ Refactor: Remove função updateNumeroCarteirinhaPaciente()
    Remove a função que utiliza long idPaciente na querlSql

--- src/main/java/Arquitetura/Dao/PacienteDAO.java
+++ src/main/java/Arquitetura/Dao/PacienteDAO.java
@@ -53,26 +53,6 @@ public class PacienteDAO {
         }
     }
 
-    public void updateNumeroCarteirinha(long idConsulta, String numCarteirinha ) throws SQLException
-    {
-        String querySql = "UPDATE Consulta "+
-                "SET numeroCarteirinha = ? "+
-                "WHERE idConsulta = ? ";
-        try(
-                Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement stmt = connection.prepareStatement(querySql))
-        {
-            stmt.setString(1, numCarteirinha);
-            stmt.setLong(2, idConsulta);
-
-            stmt.executeUpdate();
-
-        }
-        catch (Exception e) {
-            throw new SQLException("Erro ao atualizar número da carteirinha do paciente com ID: "+idConsulta, e);
-        }
-    }
-
     public boolean isCpfPaciente(String cpf) {
         String querySql = "SELECT tipoUsuario FROM Usuario WHERE cpf = ?";
 
