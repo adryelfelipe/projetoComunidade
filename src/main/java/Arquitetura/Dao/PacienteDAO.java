5a31f82d217d7fdce935c8d473acb48aa32a7629
GUSTAVO DE HONORATO PELISSARI <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-17 21:32:07 -0300

    ✨ Fit: Adiciona função findAllPacientes
    Retorna uma lista com todos os Pacientes

--- src/main/java/Arquitetura/Dao/PacienteDAO.java
+++ src/main/java/Arquitetura/Dao/PacienteDAO.java
@@ -1,13 +1,14 @@
 package Arquitetura.Dao;
 
 import Arquitetura.Config.ConnectionFactory;
+import Arquitetura.Model.Enums.Genero;
 import Arquitetura.Model.Enums.StatusPaciente;
+import Arquitetura.Model.Enums.TipoUsuario;
 import Arquitetura.Model.Paciente;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class PacienteDAO {
 
@@ -35,6 +36,65 @@ public class PacienteDAO {
         }
     }
 
+    public ArrayList<Paciente> findAllPacientes()
+    {
+        ArrayList<Paciente> listaPacientes = new ArrayList<>();
+
+        String querySql = "SELECT "+
+                "U.idUsuario, U.senha, U.nomeUsuario, U.sexo, U.cpf, U.telefone, U.email, U.dataNascimento, U.tipoUsuario, "+
+                "P.numeroCarteirinha, P.contatoEmergencia, P.idStatusPaciente, "+
+                "FROM Usuario U "+
+                "JOIN Paciente P ON U.idUsuario = P.idPaciente ";
+
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(querySql))
+        {
+            try (ResultSet resultSet = stmt.executeQuery())
+            {
+                while (resultSet.next())
+                {
+                    //long id, String nome, String cpf, String senha, Genero sexo, String telefone, String email, Date dataNascimento, String contatoEmergencia, String numeroCarterinha, StatusPaciente statusPaciente
+                    long idPaciente = resultSet.getInt("idUsuario");
+                    String nome = resultSet.getString("nomeUsuario");
+                    String cpf = resultSet.getString("cpf");
+                    String senha = resultSet.getString("senha");
+                    int idGenero = resultSet.getInt("sexo");
+                    Genero genero = switch (idGenero)
+                    {
+                        case 1 -> Genero.MASCULINO;
+                        default -> Genero.FEMININO;
+                    };
+                    String telefone = resultSet.getString("telefone");
+                    String email = resultSet.getString("email");
+                    Date datanascimento = resultSet.getDate("dataNascimento");
+                    TipoUsuario tipoUsuario = TipoUsuario.PACIENTE;
+
+                    //Atributos Paciente
+                    String contatoEmergencia = resultSet.getString("contatoEmergencia");
+                    String numCarteirinha = resultSet.getString("numeroCarteirinha");
+                    int idStatusPaciente = resultSet.getInt("idStatusPaciente");
+                    StatusPaciente statusPaciente = switch (idStatusPaciente)
+                    {
+                        case 1 -> StatusPaciente.ATIVO;
+                        default -> StatusPaciente.INATIVO;
+                    };
+
+                    Paciente paciente = new Paciente(idPaciente,nome,cpf, senha,genero, telefone, email, datanascimento, contatoEmergencia, numCarteirinha, statusPaciente);
+
+                    if(paciente != null)
+                    {
+                        listaPacientes.add(paciente);
+                    }
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao listar todos os pacientes!"+ e);
+        }
+        return listaPacientes;
+    }
     // Remoção
     public void deletarPaciente(String cpf) {
         String querySql = "DELETE p " +
