a4d2b41421a3a533add59ae742795de561efbbc7
Adryel Felipe <adryxd003@gmail.com>
2025-10-18 02:30:31 -0300

    ✨ Fit: Adiciona e implementa métodos verificadores de integridade em FuncionarioValidator
    Verificam integridade de salário e cargaHoráriaSemanal e os implementam na model Usuario

--- src/main/java/Arquitetura/Service/Validator/FuncionarioValidator.java
+++ src/main/java/Arquitetura/Service/Validator/FuncionarioValidator.java
@@ -23,4 +23,17 @@ public class FuncionarioValidator {
             throw new DadosInvalidosException("ERRO! O SALÁRIO NÃO PODE SER MENOR QUE 1500");
         }
     }
+
+    // - Métodos verificadores de integridade de dados - //
+    public void verificaIntegridadeSalario(double salario) {
+        if(salario < 0) {
+            throw new DadosInvalidosException("ERRO! O SALÁRIO NÃO PODE SER MENOR QUE 0");
+        }
+    }
+
+    public void verificaIntegridadeCargaHoraria(int cargaHorariaSemanal) {
+        if(cargaHorariaSemanal < 0) {
+            throw new DadosInvalidosException("ERRO! A CARGA HORÁRIA SEMANAL NÃO PODE SER MENOR QUE 0");
+        }
+    }
 }
