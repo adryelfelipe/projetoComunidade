79d5e0fc261c30a46f36d1a22471dce22b6c9176
EricHafemann <eric070gabriel@gmail.com>
2025-10-21 19:26:10 -0300

    ✨ Fit: Adiciona o método de atualizar o email na UsuarioDAO.

--- src/main/java/Arquitetura/Dao/UsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO.java
@@ -550,6 +550,28 @@ public class UsuarioDAO {
         }
     }
 
+    public void updateEmail (long id, String email)
+    {
+        String qurySql = "UPDATE Usuario " +
+                "SET email = ? " +
+                "WHERE idUsuario = ?";
+
+        try(Connection conn = ConnectionFactory.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(qurySql))
+        {
+
+            stmt.setString(1, email);
+            stmt.setLong(2, id);
+
+            stmt.executeUpdate();
+
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao tentar atualizar o Email do usuário.");
+        }
+    }
+
     public String cpfByID(long id) {
         String querySql = "SELECT cpf FROM Usuario WHERE idUsuario = ?";
 
