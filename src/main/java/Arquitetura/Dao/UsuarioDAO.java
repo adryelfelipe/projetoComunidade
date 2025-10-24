9580670c4a169e7a8fd3c807309b1294349a9176
gustavo1402880 <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-24 19:45:44 -0300

    ✨ Fit: Implementa função isSameTipoUsuarioem UsuarioDAO

--- src/main/java/Arquitetura/Dao/UsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO.java
@@ -879,4 +879,31 @@ public class UsuarioDAO {
         }
         return false;
     }
+    public boolean isSameTipoUsuario(long id, TipoUsuario tipoUsuario)
+    {
+        String querySql = "SELECT tipoUsuario FROM Usuario WHERE idUsuario = ? ";
+
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(querySql))
+        {
+            stmt.setLong(1,id);
+
+            try (ResultSet resultSet = stmt.executeQuery())
+            {
+                if(resultSet.next())
+                {
+                    if(tipoUsuario.getIdTipoUsuario() == resultSet.getInt(1))
+                    {
+                        return  true;
+                    }
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao verificar telefone do usuário com ID: "+ id + e);
+        }
+        return false;
+    }
 }
