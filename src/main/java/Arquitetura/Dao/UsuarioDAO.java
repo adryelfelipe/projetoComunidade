8094932557db2c285ae0b87a50d434f3242c646c
gustavo1402880 <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-10-24 19:22:27 -0300

    ✨ Fit: Implementa função isSameTelefone em UsuárioDAO

--- src/main/java/Arquitetura/Dao/UsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO.java
@@ -825,4 +825,31 @@ public class UsuarioDAO {
         }
         return false;
     }
+    public boolean isSameTelefone(long id, String telefone)
+    {
+        String querySql = "SELECT telefone FROM Usuario WHERE idUsuario = ? ";
+
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(querySql))
+        {
+            stmt.setLong(1,id);
+
+            try (ResultSet resultSet = stmt.executeQuery())
+            {
+                if(resultSet.next())
+                {
+                    if(telefone.equals(resultSet.getString(1)))
+                    {
+                        return  true;
+                    }
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao verificar telefone do usuário com ID: "+ id + e);
+        }
+        return false;
+    }
 }
