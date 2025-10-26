bbb12c96da3e8639349c0c7fe212b0caca73d65e
Adryel Felipe <adryxd003@gmail.com>
2025-10-26 16:25:55 -0300

    ♻️ Refactor: Melhora fluxo de MenuAdministrador
    Altera switch convencionais para o modelo expression e while(continuar) para while(true) com return;

--- src/main/java/Arquitetura/View/MenuUsuarios/MenuAdministrador.java
+++ src/main/java/Arquitetura/View/MenuUsuarios/MenuAdministrador.java
@@ -23,9 +23,7 @@ public class MenuAdministrador
     {
 
         int op = 0;
-        boolean continuar = true;
-
-        while (continuar) {
+        while (true) {
 
             Ferramentas.limpaTerminal();
             System.out.println("           ---------------                         Usuário: " + adm.getId());
@@ -39,7 +37,7 @@ public class MenuAdministrador
             System.out.println("| 3-Excluir Usuários |");
             System.out.println("| 4-Gerar Relatórios |");
             System.out.println("| 5-Disponibilidade  |");
-            System.out.println("| 6-Cadastro adm     |");
+            System.out.println("| 6-Cadastro ADM     |");
             System.out.println("| 7-Cadastro Medico  |");
             System.out.println("| 8-Cadastro Paciente|");
             System.out.println("| 9-Sair             |");
@@ -51,73 +49,19 @@ public class MenuAdministrador
                 MenuDefault.menuDefault();
             }
             switch (op) {
-                case 1: {
-
-                    MenuListar.ListarUsuarios(adm);
-
-                    break;
+                case 1 -> MenuListar.ListarUsuarios(adm);
+                case 2 -> MenuUpdateADM.menuUpdateInicial(adm);
+                case 3 -> MenuExcluir.ExcluirUsuario(adm);
+                case 4 -> MenuRelatorio.GerarRelatorios(adm);
+                case 5 -> MenuDisponibilidade.Disponibilidade(adm);
+                case 6 -> MenuCadastro.CriarADM(adm);
+                case 7 -> MenuCadastro.CriarMedico(adm);
+                case 8 -> MenuCadastro.CriarPaciente(adm);
+                case 9 -> {
+                    return;
                 }
-                case 2: {
-
-                    MenuUpdateADM.menuUpdateInicial(adm);
-
-                    break;
-                }
-                case 3: {
-
-                    MenuExcluir.ExcluirUsuario(adm);
-
-                    break;
-                }
-                case 4: {
-
-                    MenuRelatorio.GerarRelatorios(adm);
-
-                    break;
-                }
-                case 5:
-                {
-
-                    MenuDisponibilidade.Disponibilidade(adm);
-
-                    break;
-                }
-                case 6:
-                {
-
-                    MenuCadastro.CriarADM(adm);
-
-                    break;
-                }
-                case 7:
-                {
-
-                    MenuCadastro.CriarMedico(adm);
-
-                    break;
-                }
-                case 8:
-                {
-
-                    MenuCadastro.CriarPaciente(adm);
-
-                    break;
-                }
-                case 9: {
-
-                    continuar = false;
-
-                    break;
-                }
-                default: {
-
-                    MenuDefault.menuDefault();
-
-                    break;
-                }
+                default -> MenuDefault.menuDefault();
             }
         }
-
-        MenuInicial.Menu();
     }
 }
