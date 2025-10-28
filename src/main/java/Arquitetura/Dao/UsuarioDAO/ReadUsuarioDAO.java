773ac6f4616255a057645914029cec18ab2e091e
gustavo1402880 <gustavo_pelisari150@estudante.sesisenai.org>
2025-10-28 19:25:00 -0300

    ♻️ Refactor: Transfere a função containsTelefone de UsuarioDAO para ReadUsuarioDAO

--- src/main/java/Arquitetura/Dao/UsuarioDAO/ReadUsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO/ReadUsuarioDAO.java
@@ -423,4 +423,34 @@ public class ReadUsuarioDAO
             return false;
         }
     }
+
+    public boolean containsTelefone(String telefone)
+    {
+        //Verifica se existe aquele telefone no Banco de Dados
+        String querySql = "SELECT "+
+                "COUNT(*) "+
+                "FROM Usuario "+
+                "WHERE telefone = ? ";
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(querySql))
+        {
+            stmt.setString(1, telefone);
+
+            try (ResultSet resultSet = stmt.executeQuery();)
+            {
+                if(resultSet.next())
+                {
+                    return resultSet.getInt(1) > 0;
+                }
+
+                return false;
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao verificar telefone: "+telefone + e);
+            return false;
+        }
+    }
 }
--- src/main/java/Arquitetura/Dao/UsuarioDAO/UsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO/UsuarioDAO.java
@@ -12,37 +12,6 @@ import java.util.ArrayList;
 
 public class UsuarioDAO {
 
-
-
-    public boolean containsTelefone(String telefone)
-    {
-        //Verifica se existe aquele telefone no Banco de Dados
-        String querySql = "SELECT "+
-                "COUNT(*) "+
-                "FROM Usuario "+
-                "WHERE telefone = ? ";
-        try (
-                Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement stmt = connection.prepareStatement(querySql))
-        {
-            stmt.setString(1, telefone);
-
-            try (ResultSet resultSet = stmt.executeQuery();)
-            {
-                if(resultSet.next())
-                {
-                    return resultSet.getInt(1) > 0;
-                }
-
-                return false;
-            }
-        }
-        catch (SQLException e)
-        {
-            System.err.println("Erro ao verificar telefone: "+telefone + e);
-            return false;
-        }
-    }
     // -- UPDATES -- //
 
     public void updateNomeUsuario (long id, String novoNome)
