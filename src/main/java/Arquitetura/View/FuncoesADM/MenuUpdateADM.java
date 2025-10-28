646ab7359aac796afd7c76d0173a27c96857f30a
Adryel Felipe <adryxd003@gmail.com>
2025-10-28 12:03:27 -0300

    ✨ Fit: Adiciona a lógica do menuAutoUpdate() em MenuUpdateADM

--- src/main/java/Arquitetura/View/FuncoesADM/MenuUpdateADM.java
+++ src/main/java/Arquitetura/View/FuncoesADM/MenuUpdateADM.java
@@ -5,6 +5,7 @@ import Arquitetura.Model.Administrador;
 import Arquitetura.Model.Enums.*;
 import Arquitetura.Model.Medico;
 import Arquitetura.Model.Paciente;
+import Arquitetura.Service.AdministradorService;
 import Arquitetura.Service.MedicoService;
 import Arquitetura.Service.PacienteService;
 import Arquitetura.Service.UsuarioService;
@@ -22,10 +23,10 @@ import java.util.InputMismatchException;
 public class MenuUpdateADM {
 
     // -- Atributos -- -- //
-    private static final UsuarioValidator usuarioValidator = new UsuarioValidator();
     private static final UsuarioService usuarioService = new UsuarioService();
     private static final MedicoService medicoService = new MedicoService();
     private static final PacienteService pacienteService = new PacienteService();
+    private static final AdministradorService administradorService = new AdministradorService();
 
     public static void menuUpdateInicial(Administrador administrador) {
         // Menu
@@ -78,13 +79,6 @@ public class MenuUpdateADM {
         try {
             idPaciente = MenuEscolhaId.escolhaIdUpdate();
         } catch (IdInvalidoException e) {
-            Ferramentas.mensagemErro(e.getMessage());
-            return;
-        }
-
-        try {
-            pacienteService.idPacienteValidator(idPaciente);
-        } catch(IdInvalidoException e) {
             Ferramentas.mensagemErro(e.getMessage());
             return;
         }
@@ -213,13 +207,111 @@ public class MenuUpdateADM {
     }
 
     public static void menuAutoUpdate(Administrador administrador) {
+        // Garantia de inicialização
+        boolean verifica = false;
+        int op = 0;
+
         Ferramentas.limpaTerminal();
 
-        System.out.println("     -----------------------------");
-        System.out.println("     ---    MENU UPDATE ADM   ----");
-        System.out.println("     -----------------------------");
+        while(true) {
+            while(!verifica) {
+                System.out.println("       --------------------             --------- ATUAL --------");
+                System.out.println("       |EDITAR  A SI MESMO|             |Nome: " + administrador.getNome());
+                System.out.println("       --------------------             |CPF: " + administrador.getCpf());
+                System.out.println("                                        |Senha: " + administrador.getSenha());
+                System.out.println(" [1] - Nome                             |Email: " + administrador.getEmail());
+                System.out.println(" [2] - CPF                              |Telefone: " + administrador.getTelefone());
+                System.out.println(" [3] - Senha                            |Sexo: " + administrador.getSexo().name());
+                System.out.println(" [4] - Email                            |TipoUsuario: " + administrador.getTipoUsuario().name());
+                System.out.println(" [5] - Telefone                         |Data de Nascimento: " + administrador.getDataNascimento());
+                System.out.println(" [6] - Sexo                             |Departamento: " + administrador.getDepartamento().name());
+                System.out.println(" [7] - TipoUsuario                      ");
+                System.out.println(" [8] - Data de Nascimento               ");
+                System.out.println(" [9] - Departamento                     ");
+                System.out.println("[10] - Sair                             ");
 
+                // -- Leitura da opção -- //
+                try {
+                    op = Ferramentas.lInteiro();
+                    verifica = true;
+                } catch (InputMismatchException e){
+                    MenuDefault.menuDefault();
+                }
+            }
 
+            // Reinicia a variável de verificação
+            verifica = false;
+
+            Ferramentas.limpaTerminal();
+
+            // -- Lógica de alteração -- //
+            try {
+                switch(op) {
+                    case 1 -> {
+                        String nome = MenuSetUsuario.SetNome();
+                        usuarioService.updateNomeUsuario(administrador, administrador.getId(), nome);
+                        administrador.setNome(nome);
+                    }
+
+                    case 2 -> {
+                        String cpf = MenuSetUsuario.SetCpf();
+                        usuarioService.updateCpf(administrador, administrador.getId(), cpf);
+                        administrador.setCpf(cpf);
+                    }
+
+                    case 3 -> {
+                        String senha = MenuSetUsuario.SetSenha();
+                        usuarioService.updateSenhaUsuario(administrador, administrador.getId(), senha);
+                        administrador.setSenha(senha);
+                    }
+
+                    case 4 -> {
+                        String email = MenuSetUsuario.SetEmail();
+                        usuarioService.updateEmailUsuario(administrador, administrador.getId(), email);
+                        administrador.setEmail(email);
+                    }
+
+                    case 5 -> {
+                        String telefone = MenuSetUsuario.SetTelefone();
+                        usuarioService.updateTelefoneUsuario(administrador, administrador.getId(), telefone);
+                        administrador.setTelefone(telefone);
+                    }
+
+                    case 6 -> {
+                        Genero sexo = MenuSetUsuario.SetSexo();
+                        usuarioService.updateSexo(administrador, administrador.getId(), sexo);
+                        administrador.setSexo(sexo);
+                    }
+
+                    case 7 -> {
+                        TipoUsuario tipoUsuario = MenuSetUsuario.SetTipoUsuario();
+                        usuarioService.updateTipoUsuario(administrador, administrador.getId(), tipoUsuario);
+                        administrador.setTipoUsuario(tipoUsuario);
+                    }
+
+                    case 8 -> {
+                        Date dataNascimento = MenuSetUsuario.SetDataNascimento();
+                        usuarioService.updateDataNascimento(administrador, administrador.getId(), dataNascimento);
+                        administrador.setDataNascimento(dataNascimento);
+                    }
+
+                    case 9 -> {
+                        Departamento departamento = MenuSetAdm.SetDepartamento();
+                        administradorService.updateDepartamento(administrador, administrador.getId(), departamento);
+                        administrador.setDepartamento(departamento);
+                    }
+
+                    case 10 -> {
+                        return;
+                    }
+
+                    default -> MenuDefault.menuDefault();
+                }
+            } catch(TipoUsuarioException | DadosInvalidosException | IdInvalidoException |
+                    DataInvalidaException | CpfInvalidoException | EmailInvalidoException | TelefoneInvalidoException e) {
+                Ferramentas.mensagemErro(e.getMessage());
+            }
+        }
     }
 
     public static void menuUpdateMedico(Administrador administrador) {
@@ -234,13 +326,6 @@ public class MenuUpdateADM {
         try {
             idMedico = MenuEscolhaId.escolhaIdUpdate();
         } catch (IdInvalidoException e) {
-            Ferramentas.mensagemErro(e.getMessage());
-            return;
-        }
-
-        try {
-            medicoService.idMedicoValidator(idMedico);
-        } catch(IdInvalidoException e) {
             Ferramentas.mensagemErro(e.getMessage());
             return;
         }
@@ -278,7 +363,7 @@ public class MenuUpdateADM {
                 }
             }
 
-            // Reinicia a veriável de verificação
+            // Reinicia a variável de verificação
             verifica = false;
 
             Ferramentas.limpaTerminal();
