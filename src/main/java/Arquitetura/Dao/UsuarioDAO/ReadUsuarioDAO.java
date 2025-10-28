44239092c27330d8937a2d97d19c3aa199057816
gustavo1402880 <gustavo_pelisari150@estudante.sesisenai.org>
2025-10-28 20:36:31 -0300

    ♻️ Refactor: Transfere a função isSameSexo de UsuarioDAO para ReadUsuarioDAO

--- src/main/java/Arquitetura/Dao/UsuarioDAO/ReadUsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO/ReadUsuarioDAO.java
@@ -678,4 +678,33 @@ public class ReadUsuarioDAO
         }
         return false;
     }
+
+    // Leitura - Verifica se o sexo é igual ao existente no banco de Dados
+    public boolean isSameSexo(long id, Genero genero)
+    {
+        String querySql = "SELECT sexo FROM Usuario WHERE idUsuario = ? ";
+
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(querySql))
+        {
+            stmt.setLong(1,id);
+
+            try (ResultSet resultSet = stmt.executeQuery())
+            {
+                if(resultSet.next())
+                {
+                    if(genero.getIdGenero() == resultSet.getInt(1))
+                    {
+                        return  true;
+                    }
+                }
+            }
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao verificar telefone do usuário com ID: "+ id + e);
+        }
+        return false;
+    }
 }
--- src/main/java/Arquitetura/Dao/UsuarioDAO/UsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO/UsuarioDAO.java
@@ -187,32 +187,4 @@ public class UsuarioDAO {
             System.err.println("Erro ao tentar atualizar o sexo do Usuário.");
         }
     }
-
-    public boolean isSameSexo(long id, Genero genero)
-    {
-        String querySql = "SELECT sexo FROM Usuario WHERE idUsuario = ? ";
-
-        try (
-                Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement stmt = connection.prepareStatement(querySql))
-        {
-            stmt.setLong(1,id);
-
-            try (ResultSet resultSet = stmt.executeQuery())
-            {
-                if(resultSet.next())
-                {
-                    if(genero.getIdGenero() == resultSet.getInt(1))
-                    {
-                        return  true;
-                    }
-                }
-            }
-        }
-        catch (SQLException e)
-        {
-            System.err.println("Erro ao verificar telefone do usuário com ID: "+ id + e);
-        }
-        return false;
-    }
 }
