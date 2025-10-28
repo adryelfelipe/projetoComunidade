8e59e10670fa3e15fe85177c1b3abc3f1b36c361
gustavo1402880 <gustavo_pelisari150@estudante.sesisenai.org>
2025-10-28 18:44:59 -0300

    ♻️ Refactor: Transfere a função deletarUsuario de UsuarioDAO para DeleteUsuarioDAO

--- src/main/java/Arquitetura/Dao/UsuarioDAO/DeleteUsuarioDAO.java
+++ src/main/java/Arquitetura/Dao/UsuarioDAO/DeleteUsuarioDAO.java
@@ -8,5 +8,19 @@ import java.sql.SQLException;
 
 public class DeleteUsuarioDAO
 {
+    // Remove o usuário pelo CPF
+    public void deletarUsuario(String cpf) {
+        String querySql = "DELETE FROM Usuario WHERE cpf = ?";
 
+        try (Connection conn = ConnectionFactory.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(querySql))
+        {
+            stmt.setString(1, cpf);
+            stmt.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao deletar usuário com o CPF: "+cpf);
+        }
+    }
 }
