234081e690c32988d3d6042d1296b601d762fb8c
Adryel Felipe <adryxd003@gmail.com>
2025-10-28 12:50:00 -0300

    ✨ Fit: Implementa o método validaUpdateUsuario nos updates da MedicoService
    O método verifica se o ID recebido existe e se o usuarioInersor não é nulo

--- src/main/java/Arquitetura/Service/MedicoService.java
+++ src/main/java/Arquitetura/Service/MedicoService.java
@@ -112,38 +112,38 @@ public class MedicoService {
 
 
     public void updateEspecialidadeMedico(Usuario usuario, long id, Especialidade especialidade) {
+        usuarioService.validaUpdateUsuario(usuario, id);
         tipoUsuarioValidator.temAcessoTotal(usuario);
         medicoValidator.verificaRegrasEspecialidade(especialidade);
         usuarioService.idExistenteValidator(id);
-        idMedicoValidator(id);;
 
         medicoDAO.updateEspecialidade(id, especialidade);
     }
 
     public void updateSubEspecialidadeMedico(Usuario usuario, long id, String subEspecialidade) {
+        usuarioService.validaUpdateUsuario(usuario, id);
         tipoUsuarioValidator.temAcessoTotal(usuario);
         MedicoValidator.verificaIntegridadeSubespecialidade(subEspecialidade);
         usuarioService.idExistenteValidator(id);
-        idMedicoValidator(id);
 
         medicoDAO.updateSubEspecialidade(id, subEspecialidade);
     }
 
     public void updateFormacaoMedico(Usuario usuario, long id, String formacao) {
+        usuarioService.validaUpdateUsuario(usuario, id);
         tipoUsuarioValidator.temAcessoTotal(usuario);
         MedicoValidator.verificaIntegridadeFormacao(formacao);
         medicoValidator.verificaRegrasFormacao(formacao);
         usuarioService.idExistenteValidator(id);
-        idMedicoValidator(id);
 
         medicoDAO.updateFormacao(id, formacao);
     }
 
     public void updatePlantaoMedico(Usuario usuario, long id, Plantao plantao) {
+        usuarioService.validaUpdateUsuario(usuario, id);
         tipoUsuarioValidator.temAcessoTotal(usuario);
         medicoValidator.verificaRegrasPlantao(plantao);
         usuarioService.idExistenteValidator(id);
-        idMedicoValidator(id);
 
         medicoDAO.updatePlantao(id, plantao);
     }
