ac5eddbe0e6ef6987b33536aabb1e6dc40bd542a
GUSTAVO DE HONORATO PELISSARI <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-11-04 19:49:57 -0300

    ♻️ Refactor: Transfere a função updateCargaHorariaSemanal de FuncionarioDAO para UpdateFuncionarioDAO

--- src/main/java/Arquitetura/Dao/FuncionarioDAO/FuncionarioDAO.java
+++ src/main/java/Arquitetura/Dao/FuncionarioDAO/FuncionarioDAO.java
@@ -16,25 +16,5 @@ public class FuncionarioDAO {
 
 
 
-    public void updateCargaHorariaSemanal(long id, int cargaHorariaSemanal)
-    {
-        String querySql = "UPDATE Funcionario f"+
-                "INNER JOIN Usuario u ON f.idFuncionario = u.idUsuario "+
-                "SET cargaHorariaSemanal = ? "+
-                "WHERE idUsuario = ? ";
 
-        try (
-                Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement stmt = connection.prepareStatement(querySql))
-        {
-            stmt.setInt(1, cargaHorariaSemanal);
-            stmt.setLong(2, id);
-
-            stmt.executeQuery();
-        }
-        catch (SQLException e)
-        {
-            System.err.println("Erro ao atualizar carga horária semanal do funcionario com ID: "+id + e);
-        }
-    }
 }
