0d912040a1a8d7b02ae790dac99d54343e2b4f38
GUSTAVO DE HONORATO PELISSARI <gustavo_pelissari150@estudante.sesisenai.org.br>
2025-11-04 19:40:21 -0300

    ♻️ Refactor: Transfere a função updateSalario de FuncionarioDAO para UpdateFuncionarioDAO

--- src/main/java/Arquitetura/Dao/FuncionarioDAO/FuncionarioDAO.java
+++ src/main/java/Arquitetura/Dao/FuncionarioDAO/FuncionarioDAO.java
@@ -15,27 +15,7 @@ public class FuncionarioDAO {
 
 
 
-    public void updateSalario(long id, double salario)
-    {
-        String querySql = "UPDATE Funcionario f"+
-                "INNER JOIN Usuario u ON f.idFuncionario = u.idUsuario "+
-                "SET salario = ? "+
-                "WHERE idUsuario = ? ";
 
-        try (
-                Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement stmt = connection.prepareStatement(querySql))
-        {
-            stmt.setDouble(1, salario);
-            stmt.setLong(2, id);
-
-            stmt.executeQuery();
-        }
-        catch (SQLException e)
-        {
-            System.err.println("Erro ao atualizar salário do funcionario com ID: "+id + e);
-        }
-    }
     public void updateCargaHorariaSemanal(long id, int cargaHorariaSemanal)
     {
         String querySql = "UPDATE Funcionario f"+
--- src/main/java/Arquitetura/Dao/FuncionarioDAO/UpdateFuncionarioDAO.java
+++ src/main/java/Arquitetura/Dao/FuncionarioDAO/UpdateFuncionarioDAO.java
@@ -1,6 +1,32 @@
 package Arquitetura.Dao.FuncionarioDAO;
 
+import Arquitetura.Config.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class UpdateFuncionarioDAO
 {
+    public void updateSalario(long id, double salario)
+    {
+        String querySql = "UPDATE Funcionario f"+
+                "INNER JOIN Usuario u ON f.idFuncionario = u.idUsuario "+
+                "SET salario = ? "+
+                "WHERE idUsuario = ? ";
 
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement stmt = connection.prepareStatement(querySql))
+        {
+            stmt.setDouble(1, salario);
+            stmt.setLong(2, id);
+
+            stmt.executeQuery();
+        }
+        catch (SQLException e)
+        {
+            System.err.println("Erro ao atualizar salário do funcionario com ID: "+id + e);
+        }
+    }
 }
